2094fee7f15a54658592444f46d6e239098e8dab
Good1Cheese <2916675@gmail.com>
2024-05-09 11:28:49 +0300

    :wrench: Changed open keymap

--- lua/harpoon/buffer.lua
+++ lua/harpoon/buffer.lua
@@ -57,7 +57,7 @@ function M.setup_autocmds_and_keymaps(bufnr)
         M.run_toggle_command("Esc")
     end, { buffer = bufnr, silent = true })
 
-    vim.keymap.set("n", "<CR>", function()
+    vim.keymap.set("n", "l", function()
         M.run_select_command()
     end, { buffer = bufnr, silent = true })
 
